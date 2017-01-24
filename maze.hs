2ae59c704248b941526411c7cdf540e419ca5c4b
konstantinos <konstantinos@localhost.localdomain>
2017-01-24 17:49:03 +0200

    make maze kai derived

--- maze.hs
+++ maze.hs
@@ -4,7 +4,7 @@ import System.IO.Unsafe
 data Maze = Maze { cells :: [(Bool, Bool)]  -- [(rightWall, downWall)]
                  , width :: Int
                  , height :: Int
-                 }
+                 } deriving (Show)
 
 rand :: Int -> Int
 -- Returns a random integer from 0 to max-1
@@ -25,17 +25,22 @@ shuffleM n = do {
 
 --vazoume kai signatures
 makeMaze :: Int -> Int -> Maze
-makeMaze width height =
+makeMaze width height = Maze (fill_cell_list width height)  width height
+fill_cell_list :: Int -> Int -> [(Bool, Bool)]
+fill_cell_list width height = if (height == 1) then (fill_row width) else (fill_row width) ++
+    (fill_cell_list width (height-1))
+fill_row :: Int -> [(Bool, Bool)]
+fill_row width = if (width == 1) then [(True, True)] else [(True,True)] ++ (fill_row (width-1) )
 
 
 --prepei na diavasoume ton pseudokodika
-kruskal :: Maze -> Maze
-ksukal maze =
+--kruskal :: Maze -> Maze
+--ksukal maze =
 
 
-solvePerfect :: Maze -> (Int,Int) -> (Int, Int) -> [(Int, Int)]
-solvePerfect maze (xs,ys) (xe,ye) =
+--solvePerfect :: Maze -> (Int,Int) -> (Int, Int) -> [(Int, Int)]
+--solvePerfect maze (xs,ys) (xe,ye) =
 
 
-showMaze :: Maze -> [(Int,Int)] -> String
-showMaze maze list = 
+--showMaze :: Maze -> [(Int,Int)] -> String
+--showMaze maze list =
