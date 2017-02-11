bf03ad2c19912cbf1e1ad775db52d730473a3b85
Giannis Lam <john-lamboff@outlook.com>
2017-02-12 01:42:38 +0200

    Dromos pros ton kruskal
    den douleuoun ola akoma alla pame se kalo dromo, set to list isws?

--- maze.hs
+++ maze.hs
@@ -1,5 +1,7 @@
 import System.Random
 import System.IO.Unsafe
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 data Maze = Maze { cells :: [(Bool, Bool)]  -- [(rightWall, downWall)]
                  , width :: Int
@@ -23,8 +25,6 @@ shuffleM n = do {
                 fmap ((:) n1) $ shuffleM $ (take r n) ++ (drop (r+1) n)
              }
 
---vazoume kai signatures
---GT? (ponos skon kwlo)
 makeMaze :: Int -> Int -> Maze
 makeMaze width height = Maze (fill_cell_list width height)  width height
 
@@ -33,18 +33,48 @@ fill_cell_list width height = if (height == 1) then (fill_row width) else (fill_
     (fill_cell_list width (height-1))
 
 fill_row :: Int -> [(Bool, Bool)]
-fill_row width = if (width == 1) then [(True, True)] else [(True,True)] ++ (fill_row (width-1) )
+fill_row width = if (width == 1) then [(True, True)] else [(True, True)] ++ (fill_row (width-1) )
 
 
---prepei na diavasoume ton pseudokodika
--- !!Set.fromList as opposed to Data.Set, harder, alliws omws prepei na aknw kai import
+-- Every cell is represented by an integer equal to its position in the list 
+--POLLA THELOUN 0 ORISMA THIMISOY
 
 --kruskal :: Maze -> Maze
 --ksukal Maze =
 
--- !!Prepei na sizitisoume an ontws tah ftiaksoume data anaparastasi dentrou,
---(tha voithisei DFS alla kai kruskal) i an aplws tha to antilamvanomaste ws
---dentro (ligo skatiariko stin ilopoiisi)
+-- Function that returns a list containing a set for each cell of the maze, with a representation of it 
+init_sets :: [(Bool, Bool)] -> Int -> [Set Int]
+init_sets [] _ = []
+init_sets (c:cells) curr = (Set.singleton curr : init_sets cells (curr + 1))  
+
+-- Function that returns a list with all the possible wall positions between two neighboring cells
+init_walls :: Int -> Int -> Int -> [(Int, Int)]
+init_walls width height curr 
+	| curr `div` width < height - 1 && curr `mod` width < width - 1 = ((curr, curr + 1) : (curr, curr + width) :
+		init_walls width height (curr + 1))
+	| curr `div` width < height - 1 = ((curr, curr + width) : init_walls width height (curr + 1))
+	| curr `mod` width < width - 1 = ((curr, curr + 1) : init_walls width height (curr + 1))
+	| otherwise = []
+
+--NOT WORKING
+-- Function that replaces a set with a joined set where needed
+join_sets :: Set a -> Set a -> [Set a] -> [Set a]
+join_sets curr_joined_set joined_set sets = if Set.null curr_joined_set then sets
+	else  join_sets (Set.delete (Set.lookupGE curr_joined_set) curr_joined_set) joined_set
+		replace_nth_in_list sets (Set.lookupGE curr_joined_set) joined_set
+
+-- Function that replaces the nth set of a given list with another and returns that list
+replace_nth_in_list :: [Set a] -> Int -> Set a -> [Set a]
+replace_nth_in_list (x : xs) n new_value = if n == 0 then (new_value : xs) 
+	else (x : replace_nth_in_list xs (n - 1) new_value)
+	
+--NOT WORKING
+-- Function that executes the core kruskal algorithm part
+--place_walls :: 
+--place_walls sets [] = []
+--place_walls sets ((ci, cj) : walls) = if Set.notMember ci (sets !! cj) && Set.notMember cj (sets !! ci)
+	--then place_walls (join_sets (Set.union (sets !! ci) (sets !! cj)) sets) walls
+	--else ((ci, cj) : place_walls sets walls)
 
 
 --solvePerfect :: Maze -> (Int,Int) -> (Int, Int) -> [(Int, Int)]
